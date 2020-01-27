965a6dad44f9f3c116d8d4d0b843f4ba6713a3aa
Milo Rastgoo <hello@informagician.com>
2020-01-27 16:49:47 -0500

    next button and prev button test added

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import StarWarsCharacters from './StarWarsCharacters';
 //import {getData as mockGetData} from '../api';
 
 
 //jest.mock('../api');
 
-test('Renders the API output with API output and Previous and Next Buttons', () => {
-    
-    //mockGetData.mockResolvedValueOnce();
+test('Renders with Previous and Next Buttons', () => {
     
     const { getByText } = render(<StarWarsCharacters/>);
 
     getByText(/Previous/i);
     getByText(/Next/i);
-})
+});
+
+test('Next Button returns next URL', () => {
+
+    const { getByText } = render(<StarWarsCharacters/>);
+    const nextButton = getByText(/Next/i);
+
+    fireEvent.click(nextButton, {url:'https://swapi.co/api/people/?page=2'})
+});
+
+test('Previous Button returns null on first invo', () => {
+
+    const { getByText } = render(<StarWarsCharacters/>);
+    const prevButton = getByText(/Previous/i);
+
+    fireEvent.click(prevButton, {url:null})
+});
